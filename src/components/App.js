2db18a2713e5885f9f84060700c8a9dd0c941e27
Keith Dybvig <kdybvig@Keiths-MacBook-Pro.local>
2018-07-11 11:29:29 -0500

    changed order of pages on nav bar

--- src/components/App.js
+++ src/components/App.js
@@ -18,7 +18,7 @@ class App extends Component {
   }
 
   render() {
-    const linkNames = ['Home', 'Projects', 'About Me']
+    const linkNames = ['Home', 'About Me', 'Projects']
 
     return (
       <Router className ='router'>
