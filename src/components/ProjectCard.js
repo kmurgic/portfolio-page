bc7dedc4e1059c73daee75b5ddb3bc608550ab63
Keith Dybvig <kdybvig@Keiths-MacBook-Pro.local>
2018-07-12 11:04:08 -0500

    improved mobile compatibility of projects page

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardActions from '@material-ui/core/CardActions';
 
-const ProjectCard = (props) => {
-  const proj = props.project
+class ProjectCard extends Component {
+  state = {active: false};
 
-  const renderTools = (tools) => {
+  renderTools = (tools) => {
     const toolBoxes = tools.map((tool,index) => {
       const key = `tool-${index}`
       return <button key={key} className='tool'>{tool}</button>
@@ -15,43 +15,54 @@ const ProjectCard = (props) => {
     return toolBoxes
   }
 
-  const logo = /github/.test(proj.code) ?
-    'fa fa-github' : 'fa fa-codepen'
+  toggleActive = () => {
+    const toggledActive = !this.state.active;
+    this.setState({active: toggledActive});
+  }
 
-  return (
-    <div className='flippable'>
-      <div className='project-card'>
-        <Card raised = {true} className='card-front'>
-          <CardMedia
-           style={{paddingTop: '100%'}}
-           image = {proj.image}>
-          </CardMedia>
-          <h3 className='title-front'>{proj.title}</h3>
-        </Card>
-        <Card raised = {true} className= 'card-back'>
-          <CardContent>
-            <h3>{proj.title}</h3>
-            <p>{proj.description}</p>
-            <h4>Built With: {renderTools(proj.tools)}</h4>
-          </CardContent>
-          <CardActions className='card-actions'>
-          {proj.link &&
+  render () {
+    const proj = this.props.project;
+    const logo = /github/.test(proj.code) ?
+      'fa fa-github' : 'fa fa-codepen';
+
+    return (
+      <div
+       className={this.state.active ? 'flippable hover' : 'flippable'}
+       onClick={this.toggleActive}
+       onMouseOver= {() => this.setState({active: false})}>
+        <div className='project-card'>
+          <Card raised = {true} className='card-front'>
+            <CardMedia
+             style={{paddingTop: '100%'}}
+             image = {proj.image}>
+            </CardMedia>
+            <h3 className='title-front'>{proj.title}</h3>
+          </Card>
+          <Card raised = {true} className= 'card-back'>
+            <CardContent>
+              <h3>{proj.title}</h3>
+              <p>{proj.description}</p>
+              <h4>Built With: {this.renderTools(proj.tools)}</h4>
+            </CardContent>
+            <CardActions className='card-actions'>
+            {proj.link &&
+              <a
+               href={proj.link}
+               target='_blank'
+               rel = 'noopener noreferrer'><i className="fa fa-external-link-square"></i>
+              </a>
+            }
             <a
-             href={proj.link}
+             href={proj.code}
              target='_blank'
-             rel = 'noopener noreferrer'><i className="fa fa-external-link-square"></i>
+             rel = 'noopener noreferrer'><i className={logo}></i>
             </a>
-          }
-          <a
-           href={proj.code}
-           target='_blank'
-           rel = 'noopener noreferrer'><i className={logo}></i>
-          </a>
-          </CardActions>
-        </Card>
+            </CardActions>
+          </Card>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default ProjectCard;
