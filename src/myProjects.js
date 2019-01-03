de6b6bf59df68e2b7bdd0c022f0072c63da65932
Keith Dybvig <kdybvig@Keiths-MacBook-Pro.local>
2019-01-03 16:45:07 -0600

    updated drum-machine project description

--- src/myProjects.js
+++ src/myProjects.js
@@ -38,7 +38,7 @@ const projects = [
   {
     image: './screenshots/drum-machine.png',
     title: 'Drum Machine',
-    description: 'The drum machine allows users to play sounds by clicking on buttons or pressing down keys.  It is a freeCodeCamp project built with React.',
+    description: 'When a user clicks a button or presses a key, the drum machine plays a sound and displays the name of the sound.  It is a freeCodeCamp project built with React.',
     tools: ['HTML', 'CSS', 'React'],
     link: 'https://kdybvig.github.io/drum-machine/',
     code: 'https://github.com/kdybvig/drum-machine',
