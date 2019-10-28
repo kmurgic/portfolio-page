e165d33a114f0d72d157237d5e7dcb281682952e
Keith Murgic <keith.murgic@brandpulse.io>
2019-10-28 16:14:41 -0500

    Updated projects

--- src/myProjects.js
+++ src/myProjects.js
@@ -1,9 +1,18 @@
 const projects = [
   {
+    image: './screenshots/movieq.jpg',
+    title: 'Movie Q',
+    description: 'Movie Q is a React/Redux application that allows the user to create and re-order lists of movies.  Users can find movies by searching the movie database or discovering popular movies in any genre.',
+    tools: ['Bootstrap', 'React-Bootstrap', 'React', 'Redux', 'Redux-Saga', 'React-Router', 'React-Beautiful-DnD'],
+    link: 'https://kmurgic.github.io/movieq/',
+    code: 'https://github.com/kmurgic/movieq',
+    additionalTags: ['API']
+  },
+  {
     image: './screenshots/quizard.jpg',
     title: 'Quizard',
     description: 'Quizard is a React/Redux application that allows the user to create and play Jeopardy-style quizzes.  The user enters a title, five categories, and five questions for each category, and the application creates a Jeopardy-style game with that information.',
-    tools: ['HTML', 'CSS', 'CSS-Grid', 'Flexbox', 'SCSS', 'React', 'Redux', 'React Router'],
+    tools: ['SCSS', 'React', 'Redux', 'React Router'],
     link: 'https://kmurgic.github.io/quizard/',
     code: 'https://github.com/kmurgic/quizard',
     additionalTags: []
@@ -12,7 +21,7 @@ const projects = [
     image: './screenshots/board-game-finder.jpg',
     title: 'Board Game Finder',
     description: "Board Game Finder is an application built with React designed to help users decide which board game to purchase. Users answer a questionnaire and the application uses data from the boardgamegeek.com XML API to find the five games that best match the user's responses.",
-    tools: ['HTML', 'CSS', 'Flexbox', 'React', 'Material UI', 'XML API'],
+    tools: ['Flexbox', 'React', 'Material UI', 'XML API'],
     link: 'https://kmurgic.github.io/board-game-finder/',
     code: 'https://github.com/kmurgic/board-game-finder',
     additionalTags: ['lodash', 'API']
@@ -21,7 +30,7 @@ const projects = [
     image: './screenshots/react-checkers.jpg',
     title: 'React Checkers',
     description: 'Tournament rules checkers built with React. The board has green and cream colored squares. Red and white checker pieces are created using CSS only. Players move pieces using drag and drop.',
-    tools: ['HTML', 'CSS', 'React', 'React DnD'],
+    tools: ['React', 'React DnD'],
     link: 'https://kmurgic.github.io/react-checkers/',
     code: 'https://github.com/kmurgic/react-checkers',
     additionalTags: ['lodash', 'drag and drop', 'game', 'responsive']
@@ -30,7 +39,7 @@ const projects = [
     image: './screenshots/markdown-previewer.jpg',
     title: 'Markdown Previewer',
     description: 'Markdown Previewer allows the user to put github flavored markdown in an edit box and shows an HTML rendered preview.  The app is a freeCodeCamp project built with React and Redux.',
-    tools: ['HTML', 'CSS', 'React', 'Redux'],
+    tools: ['React', 'Redux'],
     link: 'https://kmurgic.github.io/Markdown-Previewer/',
     code: 'https://github.com/kmurgic/Markdown-Previewer',
     additionalTags: ['freeCodeCamp', 'responsive']
@@ -39,47 +48,47 @@ const projects = [
     image: './screenshots/drum-machine.jpg',
     title: 'Drum Machine',
     description: 'When a user clicks a button or presses a key, the drum machine plays a sound and displays the name of the sound.  The app is a freeCodeCamp project built with React.',
-    tools: ['HTML', 'CSS', 'React', 'Flexbox'],
+    tools: ['React'],
     link: 'https://kmurgic.github.io/drum-machine/',
     code: 'https://github.com/kmurgic/drum-machine',
     additionalTags: ['freeCodeCamp']
   },
-  {
-    image: './screenshots/wikipedia-viewer.jpg',
-    title: 'Wikipedia Viewer',
-    description: 'The Wikipedia Viewer is a freeCodeCamp project.  A user can click on the "get random article" button to see a random Wikipedia article, or search for an article on Wikipedia and see up to 12 results with descriptions and links to Wikipedia articles.',
-    tools: ['HTML', 'CSS', 'Flexbox', 'JQuery', 'REST API'],
-    link: 'https://kmurgic.github.io/wikipedia-viewer/',
-    code: 'https://github.com/kmurgic/calendartrick',
-    additionalTags: ['API', 'freeCodeCamp']
-  },
-  {
-    image: './screenshots/twitch-tv-viewer.jpg',
-    title: 'Twitch TV Viewer',
-    description: 'The Twitch TV Viewer is a freeCodeCamp project. It uses the Twitch TV API to shows users which TwitchTV channels are online and the current activity on each channel.  Users can search for a channel as well as filter by only online or only offline channels.',
-    tools: ['HTML', 'CSS', 'REST API'],
-    link: 'https://kmurgic.github.io/twitch-tv-viewer/',
-    code: 'https://github.com/kmurgic/twitch-tv-viewer',
-    additionalTags: ['API', 'freeCodeCamp']
-  },
-  {
-    image: './screenshots/simon.jpg',
-    title: 'React Simon Game',
-    description: 'The Simon Game is a freeCodeCamp project. It mimics the functionality of the original Simon. It plays a sound on every button press.  Users can play in two modes: strict and basic.  The game notifies the user when they win or lose. ',
-    tools: ['HTML', 'CSS', 'React', 'Asynchronous Code'],
-    link: false,
-    code: 'https://codepen.io/kdybvig/pen/YLOxEX',
-    additionalTags: ['freeCodeCamp', 'game']
-  },
-  {
-    image: './screenshots/calculator.jpg',
-    title: 'Javascript Calculator',
-    description: 'The Javascript Calculator is a freeCodeCamp project.  It is a four function calculator that mimics the functionality of an iPhone calculator exactly.  It is programmed using only Vanilla Javascript ',
-    tools: ['HTML', 'CSS', 'Vanilla JS'],
-    link: false,
-    code: 'https://codepen.io/kdybvig/pen/GdWNNz',
-    additionalTags: ['freeCodeCamp', 'math']
-  },
+  // {
+  //   image: './screenshots/wikipedia-viewer.jpg',
+  //   title: 'Wikipedia Viewer',
+  //   description: 'The Wikipedia Viewer is a freeCodeCamp project.  A user can click on the "get random article" button to see a random Wikipedia article, or search for an article on Wikipedia and see up to 12 results with descriptions and links to Wikipedia articles.',
+  //   tools: ['HTML', 'CSS', 'Flexbox', 'JQuery', 'REST API'],
+  //   link: 'https://kmurgic.github.io/wikipedia-viewer/',
+  //   code: 'https://github.com/kmurgic/calendartrick',
+  //   additionalTags: ['API', 'freeCodeCamp']
+  // },
+  // {
+  //   image: './screenshots/twitch-tv-viewer.jpg',
+  //   title: 'Twitch TV Viewer',
+  //   description: 'The Twitch TV Viewer is a freeCodeCamp project. It uses the Twitch TV API to shows users which TwitchTV channels are online and the current activity on each channel.  Users can search for a channel as well as filter by only online or only offline channels.',
+  //   tools: ['HTML', 'CSS', 'REST API'],
+  //   link: 'https://kmurgic.github.io/twitch-tv-viewer/',
+  //   code: 'https://github.com/kmurgic/twitch-tv-viewer',
+  //   additionalTags: ['API', 'freeCodeCamp']
+  // },
+  // {
+  //   image: './screenshots/simon.jpg',
+  //   title: 'React Simon Game',
+  //   description: 'The Simon Game is a freeCodeCamp project. It mimics the functionality of the original Simon. It plays a sound on every button press.  Users can play in two modes: strict and basic.  The game notifies the user when they win or lose. ',
+  //   tools: ['HTML', 'CSS', 'React', 'Asynchronous Code'],
+  //   link: false,
+  //   code: 'https://codepen.io/kdybvig/pen/YLOxEX',
+  //   additionalTags: ['freeCodeCamp', 'game']
+  // },
+  // {
+  //   image: './screenshots/calculator.jpg',
+  //   title: 'Javascript Calculator',
+  //   description: 'The Javascript Calculator is a freeCodeCamp project.  It is a four function calculator that mimics the functionality of an iPhone calculator exactly.  It is programmed using only Vanilla Javascript ',
+  //   tools: ['HTML', 'CSS', 'Vanilla JS'],
+  //   link: false,
+  //   code: 'https://codepen.io/kdybvig/pen/GdWNNz',
+  //   additionalTags: ['freeCodeCamp', 'math']
+  // },
   /*{
     image: './screenshots/towers-of-hanoi.jpg',
     title: 'Towers Of Hanoi',
